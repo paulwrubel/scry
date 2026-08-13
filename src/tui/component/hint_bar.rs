dd14885d328553fe683d172d7892950943374845
Paul LeClair <pwrubel7@gmail.com>
2026-08-13 00:06:54 -0500

    refactor hint bar

--- src/tui/component/hint_bar.rs
+++ src/tui/component/hint_bar.rs
@@ -1,6 +1,6 @@
 use crate::tui::component::RenderContext;
-use ratatui::style::{Modifier, Style};
-use ratatui::widgets::Paragraph;
+use ratatui::style::{Modifier, Style, Stylize};
+use ratatui::text::{Line, Span};
 
 pub struct HintBar {
     pub message: String,
@@ -19,20 +19,31 @@ impl HintBar {
 
     pub fn render(&self, ctx: &mut RenderContext) {
         let hint_actions = [
-            "[a]dd task",
-            "[m]ove task",
-            "[d]elete task",
-            "[Enter]: task details",
+            ["[a]", "dd task"],
+            ["[m]", "ove task"],
+            ["[d]", "elete task"],
+            ["[Enter]", " task details"],
         ];
-        let (text, style) = if self.message.is_empty() {
-            (hint_actions.join(" | "), Style::default())
+
+        let spans = if self.message.is_empty() {
+            hint_actions
+                .into_iter()
+                .flat_map(|[key, desc]| {
+                    [
+                        Span::styled(" • ", Style::default().dim()),
+                        Span::styled(key, Style::default()),
+                        Span::styled(desc, Style::default().dim()),
+                    ]
+                })
+                .skip(1)
+                .collect::<Vec<Span>>()
         } else {
-            (
+            vec![Span::styled(
                 self.message.clone(),
                 Style::default().add_modifier(Modifier::BOLD),
-            )
+            )]
         };
 
-        ctx.render_widget(Paragraph::new(text).style(style));
+        ctx.render_widget(Line::from(spans));
     }
 }
